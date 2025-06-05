ae865b2392618d7b93bebba1080fcee84b087f34
Martin <maospital@itba.edu.ar>
2025-06-05 13:19:08 -0300

    bbb

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 let listadoTareas= []
-const tareaInput= document.getElementById('tarea')
-const descInput= document.getElementById('desc')
 
 // Funcion Validacion Datos Tarea
-
 function validacion(tarea, desc) {
     if (tarea == '' || desc == '') {
         alert('Ingrese bien los datos de la tarea')
@@ -13,10 +10,11 @@ function validacion(tarea, desc) {
 }
 
 // Funcion Agregar Tarea (+ boton editar, + boton eliminar)
-
-document.querySelector('#agregar').addEventListener('click', function (event) {
+document.querySelector('#agregarBtn').addEventListener('click', function (event) {
     event.preventDefault()
 
+    const tareaInput= document.getElementById('tarea')
+    const descInput= document.getElementById('desc')
     const tarea= tareaInput.value.trim()
     const desc= descInput.value.trim()
 
@@ -26,8 +24,85 @@ document.querySelector('#agregar').addEventListener('click', function (event) {
         return
     }
 
-
+    listadoTareas.push({tarea, desc})
+    tareaInput.value= ''
+    descInput.value= ''
+    mostrarLista()
 })
 
+// Funcion Eliminar Tarea
+function eliminarTarea(index) {
+    listaMascotas.splice(index, 1)
+    mostrarLista()
+}
 
-document.querySelector
+// Funcion Editar Tarea
+function editarTarea(index) {
+    const tareaInput = document.getElementById('tarea')
+    const descInput = document.getElementById('desc')
+    const agregarBtn = document.querySelector('#agregarBtn')
+
+    // El formulario se llena con los datos actuales y oculto btnAgregar
+    tareaInput.value = listadoTareas[index].tarea
+    descInput.value = listadoTareas[index].desc
+    agregarBtn.style.display = 'none'
+
+    if (!document.getElementById('guardarCambiosBtn')) {
+        const btnGuardar = document.createElement('button')
+        btnGuardar.textContent = 'Guardar cambios'
+        btnGuardar.id = 'btnGuardarCambios'
+
+        btnGuardar.addEventListener('click', function (e) {
+            e.preventDefault()
+
+            const nuevaTarea = tareaInput.value.trim()
+            const nuevaDesc = descInput.value.trim()
+
+            if (!validacion(nuevaTarea, nuevaDesc)) {
+                return
+            }
+
+            listadoTareas[index] = { tarea: nuevaTarea, desc: nuevaDesc }
+
+            tareaInput.value = ''
+            descInput.value = ''
+            mostrarLista()
+
+            // Muestro btnAgregar, saco btnGuardar
+            btnGuardar.remove()
+            agregarBtn.style.display = 'inline'
+        })
+
+        // Agregamos el botón al formulario
+        document.getElementById('agregar').appendChild(btnGuardar)
+    }
+}
+
+// Funcion Mostrar Lista (para actualizar bien las tareas)
+function mostrarLista() {
+    let lista= document.querySelector('#listaTareas')
+    lista.innerHTML= ''
+      
+    for (let i= 0; i < listadoTareas.length; i++) {
+        let task= listadoTareas[i]
+        let li= document.createElement('li')
+        li.innerHTML= `<b>Tarea:</b> ${task.tarea}, 
+        <b>Detalles:</b> ${task.desc}  `
+        
+        const btnEditar= document.createElement('button')
+        btnEditar.textContent= 'Editar'
+        btnEditar.addEventListener('click', () => {
+          editarTarea(i)
+        })
+        
+        const btnEliminar= document.createElement('button')
+        btnEliminar.textContent= 'Eliminar'
+        btnEliminar.addEventListener('click', () => {
+          eliminarTarea(i)
+        })
+        
+        li.appendChild(btnEditar)
+        li.appendChild(btnEliminar)
+        lista.appendChild(li)
+    }
+}
